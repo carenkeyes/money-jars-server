6d2d5b3d128ecfa6cfcd92a7fba7629a242cfa7f
Caren Keyes <cejkeyes@gmail.com>
2018-07-20 11:12:45 -0700

    fix typos

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-const express = require('expres')
+const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.get('/api/*', (req, res) => {
-    res.json({ok: true});
+  res.json({ok: true});
 });
 
-app.listen(PORT, () => console.log('Listening on port ${PORT}'));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
-module.export = {app}
+module.exports = {app};
