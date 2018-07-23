62046d1e04e858685147745eab3327ebd5ef2f3e
Caren Keyes <cejkeyes@gmail.com>
2018-07-23 08:49:55 -0700

    add open and close server functions

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
+require('.dotenv').config();
+
 const express = require('express');
+const morgan = require('morgan');
 const cors = require('cors');
+
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
 const app = express();
+const {PORT, CLIENT_ORIGIN, DATABASE_URL} = require('./config')
 
-const PORT = process.env.PORT || 3000;
-const {CLIENT_ORIGIN} = require('./config')
 
+app.use(express.static('public'));
+app.use(morgan('common'))
+app.use(express.json());
 app.use(
   cors({
     origin: CLIENT_ORIGIN
@@ -15,6 +25,44 @@ app.get('/api/*', (req, res) => {
   res.json({ok: true});
 });
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+let server;
 
-module.exports = {app};
+function runServer(databaseUrl, port = PORT){
+    return new Promise((resolve, reject) => {
+        mongoose.connect(databaseUrl, err =>{
+            if (err) {
+                return Promise.reject(err);
+            }
+            server = app.listen(port, () =>{
+                console.log(`Your app is listening on port ${port}`);
+                resolve();
+            })
+        
+            .on('error', err => {
+                mongoose.disconnect();
+                reject(err);
+            });
+        });
+    });
+}
+
+
+function closeServer(){
+    return mongoose.disconnect().then(() => {
+        return new Promise((resolve, reject) =>{
+            console.log('Closing server');
+            server.close(err => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve();
+            });
+        });
+    });
+}
+
+if (require.main === module) {
+    runServer(DATABASE_URL).catch(err => console.error(err));
+  }
+  
+module.exports = { runServer, app, closeServer };
