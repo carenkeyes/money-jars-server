cc7f9003c787ef7ebd3b478e55270d56c05a194a
Caren Keyes <cejkeyes@gmail.com>
2018-07-25 22:49:28 -0700

    work on proxy router for ynab

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-money-jars',
     PORT: process.env.PORT || 8080,
     CLIENT_SECRET: process.env.CLIENT_SECRET,
-    REDIRECT_URI: process.env.REDIRECT_URI || 'http://localhost:8080/api/ynab/auth',
+    REDIRECT_URI: process.env.REDIRECT_URI || 'urn:ietf:wg:oauth:2.0:oob',
     CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
     OAUTH_CODE: process.env.OAUTH_CODE
 }
--- routers/ynabRouter.js
+++ routers/ynabRouter.js
@@ -1,3 +1,5 @@
+const USER_INFO = {};
+
 const express = require('express');
 const router = express.Router();
 const request = require('superagent');
@@ -8,12 +10,13 @@ const jsonParser = bodyParser.json();
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 
-const {CLIENT_SECRET, REDIRECT_URI, OAUTH_CODE} = require('../config');
-//Oauth code will come from database storage once the database is set up
+const {CLIENT_SECRET, REDIRECT_URI} = require('../config');
+//Oauth code will come from authorization request
+//This is for development only
 
 
 const ynab = require('ynab');
-let accessToken;
+let accessToken = '15b48ce22e763a8dda4c471d06d7686fecb3e517bbf9d98792ed283df39fdcf7'
 const ynabAPI = new ynab.API(accessToken);
 
 mongoose.Promise = global.Promise;
@@ -28,25 +31,102 @@ router.get('/', (req, res) => {
 });
 
 router.get('/budgets', (req, res) => {
-    
-    /*let getToken = function(){
-        request
-            .post('https://app.youneedabudget.com/oauth/token')
-            .send({
+    let budgetList;
+    console.log(USER_INFO)
+    console.log(USER_INFO.created_at);
+    console.log(USER_INFO.expires_in);
+    console.log(Date.now());
 
-            })
+    /*if (USER_INFO.created_at + USER_INFO.expires_in < Date.now()){
+        console.log(Date.now())
+        console.log('token expired');
+        refreshToken()
     }*/
 
+    ynabAPI.budgets
+        .getBudgets()
+        .then(response => {
+            budgetList = renderBudgets(response)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+
+    console.log(budgetList);
+    res.json({status: 'budgets'});
+
 })
 
+function renderBudgets(results){
+    console.log(results.data.budgets[0].name);
+    console.log(results.data.budgets.length);
+    const budgetList = []
+    for(let i=0; i<results.data.budgets.length; i++){    
+            console.log(results.data.budgets[i].name)
+            console.log(results.data.budgets[i].id)
+            budgetList.push({
+                //name:results.data.budgets[i].name,
+                //id:results.data.budgets[i].id
+                i
+            })
+    }
+    console.log(`budgetList: ${budgetList}`)
+    return budgetList
+
+}
+
+//For production, add a visit to the authorization page first, then
+//get the access token 
+//They are two steps currently because of development limitations
 router.post('/auth', (req, res) => {
     console.log('redirect worked')
-
-    getToken(req.params.code)
+    let code = "95db61b01a1072766f8bf79622aae449a8134963c79b0afbe1368d0538368ea3"   
+    getToken(code)
 })
 
-function getToken(response){
-    console.log('get token ran');
+function getToken(code){
+    request
+        .post('https://app.youneedabudget.com/oauth/token')
+        .send({
+            client_id: "524cb6ed48eb7037b8391bc45974590dace8e9b2434cc03e5ae595b54412cced",
+            client_secret: `${CLIENT_SECRET}`,
+            redirect_uri: "urn:ietf:wg:oauth:2.0:oob",
+            grant_type: "authorization_code",
+            code: `${code}`
+        })
+        .then(res => {
+            USER_INFO.access_token = res.body.access_token
+            USER_INFO.expires_in = res.body.expires_in
+            USER_INFO.refresh_token = res.body.refresh_token
+            USER_INFO.created_at = res.body.created_at
+            console.log(USER_INFO)
+        })
+        .catch(function(err){
+            console.log(err.message)
+        })
+    
+}
+
+function refreshToken(){
+    console.log('refresh token ran')
+    request
+        .post('https://app.youneedabudget.com/oauth/token')
+        .send({
+            client_id: "524cb6ed48eb7037b8391bc45974590dace8e9b2434cc03e5ae595b54412cced",
+            client_secret: `${CLIENT_SECRET}`,
+            grant_type: 'refresh_token',
+            refresh_token: `${USER_INFO.refresh_token}`
+        })
+        .then(res => {
+            USER_INFO.access_token = res.body.access_token
+            USER_INFO.expires_in = res.body.expires_in
+            USER_INFO.refresh_token = res.body.refresh_token
+            USER_INFO.created_at = res.body.created_at
+            console.log(USER_INFO)
+        })
+        .catch(function(err){
+            console.log(err.message)
+        })
 }
 
 module.exports = router;
