3957f806fcf0abcdda674c3839fb07ea3ae5f403
Caren Keyes <cejkeyes@gmail.com>
2018-07-26 11:55:44 -0700

    add async function for retrieving ynab budget list

--- routers/ynabRouter.js
+++ routers/ynabRouter.js
@@ -1,4 +1,9 @@
-const USER_INFO = {};
+const USER_INFO = {
+    access_token: '42c06c8bb073d584fc8175f7366fa3585fe4fd516ad1aa194ea84da64ff588b2',
+    expires_in: 7200,
+    refresh_token:'22206ec766d7d4e7ddc18b91151423d5bc8c9458aa16a404488dae983ddffbd4',
+    created_at: 1532628401 
+};
 
 const express = require('express');
 const router = express.Router();
@@ -16,7 +21,7 @@ const {CLIENT_SECRET, REDIRECT_URI} = require('../config');
 
 
 const ynab = require('ynab');
-let accessToken = '15b48ce22e763a8dda4c471d06d7686fecb3e517bbf9d98792ed283df39fdcf7'
+let accessToken = USER_INFO.access_token;
 const ynabAPI = new ynab.API(accessToken);
 
 mongoose.Promise = global.Promise;
@@ -31,49 +36,56 @@ router.get('/', (req, res) => {
 });
 
 router.get('/budgets', (req, res) => {
-    let budgetList;
-    console.log(USER_INFO)
-    console.log(USER_INFO.created_at);
-    console.log(USER_INFO.expires_in);
-    console.log(Date.now());
 
-    /*if (USER_INFO.created_at + USER_INFO.expires_in < Date.now()){
-        console.log(Date.now())
+    console.log(`created: ${USER_INFO.created_at}`)
+    console.log(`expires: ${USER_INFO.expires_in}`)
+    console.log(`current date: ${Math.floor(Date.now()/1000)}`);
+
+    if (USER_INFO.created_at + USER_INFO.expires_in < Math.floor(Date.now()/1000)){
         console.log('token expired');
         refreshToken()
-    }*/
+    } else{
+        retrieveBudgets()   
+    }
 
-    ynabAPI.budgets
-        .getBudgets()
-        .then(response => {
-            budgetList = renderBudgets(response)
-        })
-        .catch(err => {
-            console.log(err)
-        })
-
-    console.log(budgetList);
     res.json({status: 'budgets'});
 
 })
 
-function renderBudgets(results){
+async function retrieveBudgets(response){
+const budgetList = []
+
+    try{
+        const budgetsResponse = await ynabAPI.budgets.getBudgets();
+        const budgets = budgetsResponse.data.budgets;
+        for (let budget of budgets){
+            budgetList.push(`[name: ${budget.name}, [id: ${budget.id}]`)
+        }
+    } catch (e) {
+        console.log(`error: ${JSON.stringify(e)}`);
+    }
+
+    console.log(budgetList)
+
+}
+
+/*function renderBudgets(results){
     console.log(results.data.budgets[0].name);
     console.log(results.data.budgets.length);
-    const budgetList = []
+    const list = []
+
     for(let i=0; i<results.data.budgets.length; i++){    
             console.log(results.data.budgets[i].name)
             console.log(results.data.budgets[i].id)
-            budgetList.push({
-                //name:results.data.budgets[i].name,
+            list.push(
+                results.data.budgets[i].name,
                 //id:results.data.budgets[i].id
-                i
-            })
+            )
     }
-    console.log(`budgetList: ${budgetList}`)
-    return budgetList
+    console.log(`list of budgets: ${list}`)
+    return list
 
-}
+}*/
 
 //For production, add a visit to the authorization page first, then
 //get the access token 
@@ -123,6 +135,7 @@ function refreshToken(){
             USER_INFO.refresh_token = res.body.refresh_token
             USER_INFO.created_at = res.body.created_at
             console.log(USER_INFO)
+            retrieveBudgets();
         })
         .catch(function(err){
             console.log(err.message)
