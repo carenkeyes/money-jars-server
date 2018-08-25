e32f538ed17204e65643909dc2e09e980ed3873b
Caren Keyes <cejkeyes@gmail.com>
2018-08-25 09:21:00 -0700

    add model to store ynab auth information

---
+++ src/models/ynabModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+require('mongoose-type-email');
+mongoose.Promise = global.Promise;
+
+const accountSchema = new mongoose.Schema({
+    budget_id: String,
+    access_token: String,
+    expires_in: Number,
+    refresh_token: String,
+    created_at: Number
+})
+
+module.exports = mongoose.model('Account', accountSchema);
